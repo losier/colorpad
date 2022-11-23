7d406d957c405796e7d2d5bcf05a9240889c4181
ZZCWC <85448514+losier@users.noreply.github.com>
2022-11-23 20:59:08 +0530

    initial template

---
+++ components/Body.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../styles/body.module.css";
+
+const Body = () => {
+  return (
+    <div>
+      <main className={styles.main}></main>
+    </div>
+  );
+};
+
+export default Body;
