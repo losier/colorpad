5d29d123e003c9bd7b3a2e0b56d17f41c3753850
ZZCWC <85448514+losier@users.noreply.github.com>
2022-11-23 21:32:15 +0530

    added basic html

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/form.module.css";
 
 const Form = () => {
+  const [color, setColor] = useState("#000000");
+  const [h, setH] = useState("");
+  const [s, setS] = useState("");
+  const [l, setL] = useState("");
+  const [r, setR] = useState("");
+  const [g, setG] = useState("");
+  const [b, setB] = useState("");
+
   return (
-    <div>
-      <form></form>
+    <div style={{ backgroundColor: color }}>
+      <div className={styles.color_container}>
+        <input
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+      </div>
+
+      {/* HEX  */}
+      <div className={styles.hex}>
+        <article>
+          <input type="number" onChange={(e) => setH(e.target.value)} />
+          <h1>H</h1>
+        </article>
+        <article>
+          <input type="number" onChange={(e) => setS(e.target.value)} />
+          <h1>S</h1>
+        </article>
+        <article>
+          <input type="number" onChange={(e) => setL(e.target.value)} />
+          <h1>L</h1>
+        </article>
+      </div>
+
+      {/* RGB  */}
+      <div className={styles.rgb}>
+        <article>
+          <input type="number" onChange={(e) => setR(e.target.value)} />
+          <h1>R</h1>
+        </article>
+        <article>
+          <input type="number" onChange={(e) => setG(e.target.value)} />
+          <h1>G</h1>
+        </article>
+        <article>
+          <input type="number" onChange={(e) => setB(e.target.value)} />
+          <h1>B</h1>
+        </article>
+      </div>
     </div>
   );
 };
