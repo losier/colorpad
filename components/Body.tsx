c396533c19a1090c28c9aff1705770a45cd4e57a
ZZCWC <85448514+losier@users.noreply.github.com>
2022-11-23 21:03:38 +0530

    updated body

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import styles from "../styles/body.module.css";
 
+// importing components
+import Form from "./Form";
+import Footer from "./Footer";
+
 const Body = () => {
   return (
     <div>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <div className={styles.form_container}>
+          <Form />
+        </div>
+        <footer>
+          <Footer />
+        </footer>
+      </main>
     </div>
   );
 };
