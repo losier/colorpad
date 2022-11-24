c13c1f23ddce723379819a156c2999bed3b54a86
ZZCWC <85448514+losier@users.noreply.github.com>
2022-11-24 22:24:12 +0530

    added hext to rgb

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import styles from "../styles/form.module.css";
-import jsonData from "../assets/color.json";
 
 const Form = () => {
   const [color, setColor] = useState("#f05600");
@@ -10,6 +9,16 @@ const Form = () => {
   const [r, setR] = useState("");
   const [g, setG] = useState("");
   const [b, setB] = useState("");
+
+  const hexTOrgb = (hex: string) => {
+    var r = parseInt(hex.slice(1, 3), 16);
+    var g = parseInt(hex.slice(3, 5), 16);
+    var b = parseInt(hex.slice(5, 7), 16);
+
+    setR(`${r}`);
+    setB(`${b}`);
+    setG(`${g}`);
+  };
 
   return (
     <div style={{ backgroundColor: color }} className={styles.main_container}>
@@ -27,9 +36,11 @@ const Form = () => {
           onChange={(e) => {
             if (e.target.value.startsWith("#")) {
               setColor(e.target.value);
+              hexTOrgb(color);
             }
             if (e.target.value.length === 0) {
               setColor("#000");
+              hexTOrgb("#000000");
             }
           }}
         />
@@ -38,32 +49,44 @@ const Form = () => {
       {/* HEX  */}
       <div className={styles.hsl}>
         <article>
-          <input type="number" onChange={(e) => setH(e.target.value)} />
-          <h1>H</h1>
+          <input type="number" onChange={(e) => {}} value={h} />
+          <h1>Hue</h1>
         </article>
         <article>
           <input type="number" onChange={(e) => setS(e.target.value)} />
-          <h1>S</h1>
+          <h1>Saturation</h1>
         </article>
         <article>
           <input type="number" onChange={(e) => setL(e.target.value)} />
-          <h1>L</h1>
+          <h1>Lightness</h1>
         </article>
       </div>
 
       {/* RGB  */}
       <div className={styles.rgb}>
         <article>
-          <input type="number" onChange={(e) => setR(e.target.value)} />
-          <h1>R</h1>
+          <input
+            type="number"
+            onChange={(e) => setR(e.target.value)}
+            value={r}
+          />
+          <h1>Red</h1>
         </article>
         <article>
-          <input type="number" onChange={(e) => setG(e.target.value)} />
-          <h1>G</h1>
+          <input
+            type="number"
+            onChange={(e) => setG(e.target.value)}
+            value={g}
+          />
+          <h1>Green</h1>
         </article>
         <article>
-          <input type="number" onChange={(e) => setB(e.target.value)} />
-          <h1>B</h1>
+          <input
+            type="number"
+            onChange={(e) => setB(e.target.value)}
+            value={b}
+          />
+          <h1>Blue</h1>
         </article>
       </div>
 
@@ -76,6 +99,7 @@ const Form = () => {
               16
             );
             setColor("#" + randomColor);
+            hexTOrgb("#" + randomColor);
           }}
           className={styles.btn}
         >
